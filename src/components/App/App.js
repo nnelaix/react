4a7f24e9ad46a723ec0342027b4607c1f6ec7b20
Agata Lewandowska <lewandowskaagata15@gmail.com>
2021-02-28 20:05:07 +0100

    15.3 add Hero and List

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import styles from './App.scss';
+import List from '../List/List.js';
+import Hero from '../Hero/Hero.js';
 
 class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
-        <h1>My first React app</h1>
-        <h2>Hello World!</h2> 
+        <h1 className={styles.title}>My first React app</h1>
+        <h2 className={styles.subtitle}>Hello World!</h2> 
+        <List />
+        <Hero />
       </main>
     )
   }
