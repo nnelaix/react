f91c54ddf15a019604ba3af6f9d8fb4ca64df32b
Agata Lewandowska <lewandowskaagata15@gmail.com>
2021-02-28 19:45:39 +0100

    Basic files config

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main className={styles.component}>
+        <h1>My first React app</h1>
+        <h2>Hello World!</h2> 
+      </main>
+    )
+  }
+}
+
+export default App;
