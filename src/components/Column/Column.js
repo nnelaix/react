01d4b35f7ec64a19d3a6cea4d68fcf779cb6b73e
Agata Lewandowska <lewandowskaagata15@gmail.com>
2021-03-01 15:27:06 +0100

    Column props static declaration

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,11 +3,14 @@ import styles from './Column.scssx';
 import PropTypes from 'prop-types';
 
 class Column extends React.Component {
+  static propTypes = {
+    columnTitle: PropTypes.string.isRequired,
+  }
+
   render() {
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>{this.props.columnTitle}</h3> 
-        
       </section>
     )
   }
