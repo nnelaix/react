2b4706736aaf57232c2ea1f2f2fc65020b17aa0c
Agata Lewandowska <lewandowskaagata15@gmail.com>
2021-03-19 18:44:59 +0100

    card selector change, action type, reducer

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -3,7 +3,7 @@ import shortid from 'shortid';
 // selectors
 export const getCardsForList = ({cards}, listId) => cards.filter(card => card.listId == listId);
 
-export const getCardsForColumn = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
+export const getCardsForColumn = ({cards, searchString}, columnId) => cards.filter(card => card.columnId == columnId && new RegExp(searchString, 'i').test(card.title));
 
 
 // action name creator
--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -4,16 +4,22 @@ export const getSearchString = state => state.searchString;
 
 export const countAllCards = ({cards}) => cards.length;
 
-export const countVisibleCards = ({cards}) => cards.length.filter(card => new RegExp(searchString, 'i').test(card.title));
+export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 // action name creator
 
+const reducerName = 'searchString';
+const createActionName = name => `app/${reducerName}/${name}`;
 // actions types
+export const CHANGE = createActionName('CHANGE');
 
 // action creators
+export const ccreateAction_changeSearchString = payload => ({ payload: { ...payload, id: shortid.generate() }, type: CHANGE});
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case CHANGE:
+      return [action.payload];
     default:
       return statePart;
   }
