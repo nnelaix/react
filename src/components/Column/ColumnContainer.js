641b4b01fe4c5a29383b68171c937c85e6665c2a
Agata Lewandowska <lewandowskaagata15@gmail.com>
2021-03-23 13:32:16 +0400

    16.4 fix add card

--- src/components/Column/ColumnContainer.js
+++ src/components/Column/ColumnContainer.js
@@ -10,7 +10,7 @@ const mapStateToProps = (state, props) => ({
 
 const mapDispatchToProps = (dispatch, props) => ({
   addCard: title => dispatch(createActionAddCard({
-    listId: props.id,
+    columnId: props.id,
     title,
   })),
 });
